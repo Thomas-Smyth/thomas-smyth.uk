63047af10ed13e4efc014cf0a2fd1f745bbded07
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-09-12 00:18:19 +0100

    Add SquadJS live data endpoint

--- src/subdomains/squadjs/api/v1/live.ts
+++ src/subdomains/squadjs/api/v1/live.ts
@@ -1,5 +1,19 @@
 import Koa from 'koa';
 
+import { SquadJSInstances } from '../../lib/database';
+
 export const live = async (ctx: Koa.BaseContext): Promise<void> => {
-  ctx.body = { servers: null, players: null };
+  ctx.body = {
+    servers: await SquadJSInstances.countDocuments(),
+    players: await (
+      await SquadJSInstances.aggregate([
+        {
+          $group: {
+            _id: '',
+            playerCount: { $sum: '$playerCount' },
+          },
+        },
+      ]).toArray()
+    )[0]?.playerCount,
+  };
 };
