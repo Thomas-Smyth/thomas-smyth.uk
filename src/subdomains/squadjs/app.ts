0d7adce3de9a700659c878bcb63d84c9a118307a
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-09-11 22:58:19 +0100

    Add SquadJS redirects

--- src/subdomains/squadjs/app.ts
+++ src/subdomains/squadjs/app.ts
@@ -5,6 +5,14 @@ export const initSubdomain = (): Koa => {
   const app = new Koa();
   const router = new Router();
 
+  router.get('/', (ctx) => {
+    ctx.redirect('https://github.com/Thomas-Smyth/SquadJS');
+  });
+
+  router.get('/discord', (ctx) => {
+    ctx.redirect('https://discord.gg/DjrpPuw');
+  });
+
   // Define a mock route temporarily.
   router.post('/api/v1/ping', (ctx) => {
     ctx.body = { message: 'Pong.' };
