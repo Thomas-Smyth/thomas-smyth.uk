ab227f3ecac661dee9b1813286da65f66dbb2d9c
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-09-12 10:30:43 +0100

    Add time filter to SquadJS live endpoint

--- src/subdomains/squadjs/api/v1/live.ts
+++ src/subdomains/squadjs/api/v1/live.ts
@@ -3,10 +3,17 @@ import Koa from 'koa';
 import { SquadJSInstances } from '../../lib/database';
 
 export const live = async (ctx: Koa.BaseContext): Promise<void> => {
+  const withinHours = ctx.request.query.withinHours ? parseInt(ctx.request.query.withinHours) : 1;
+
   ctx.body = {
-    servers: await SquadJSInstances.countDocuments(),
+    servers: await SquadJSInstances.countDocuments({
+      lastPinged: { $gte: withinHours * 60 * 60 * 1000 },
+    }),
     players: await (
       await SquadJSInstances.aggregate([
+        {
+          $match: { lastPinged: { $gte: withinHours * 60 * 60 * 1000 } },
+        },
         {
           $group: {
             _id: '',
